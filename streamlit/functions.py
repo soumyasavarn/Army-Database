bbb333d08d4e4678e201abab727314d61c31b8be
Soumya Savarn <soumyasavarn2@gmail.com>
2024-06-17 01:19:21 +0530

    Update functions.py

--- streamlit/functions.py
+++ streamlit/functions.py
@@ -8,6 +8,7 @@ host_name = "army-database-army-database.j.aivencloud.com"
 user_name = "avnadmin"
 user_password = "AVNS__umkEfKeGkBKQ4UL31v" 
 db_name = "defaultdb"
+timeout=10
 
 #commit function
 def add_officer(uid, officer_name, officer_rank, officer_unit, married, accomodation, mess_member, guest):
